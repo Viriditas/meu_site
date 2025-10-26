ae5c6789c2abc454210ce0c4bf131aed261e51be
ViriditaS <matheus1.9gama@gmail.com>
2025-10-26 00:39:23 -0300

    Adição da importação eventlet

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, send
 from datetime import datetime
